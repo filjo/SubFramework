13b7b09f4728c99ac09d32191adf91d678943818
Petar Filev <Petar.Filev@netcetera.com>
2018-11-09 14:15:54 +0100

    New .framework file

--- SubFramework.framework/Headers/SubFramework.h
+++ SubFramework.framework/Headers/SubFramework.h
@@ -16,4 +16,3 @@ FOUNDATION_EXPORT const unsigned char SubFrameworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SubFramework/PublicHeader.h>
 
-
